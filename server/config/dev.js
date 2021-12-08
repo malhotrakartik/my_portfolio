ef1373bf92ec11618794523bb8317497e173287c
Kartik Malhotra <kartikmalhotra448@gmail.com>
2021-12-08 11:29:55 +0530

    master created

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,3 @@
-// mongodb+srv://malhotra_kartik:<password>@portfoliodata.n9jap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 module.exports={
     MONGOURI : "mongodb+srv://malhotra_kartik:O8darp9Xscwka6O8@portfoliodata.n9jap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-
 }
